a532fe40447b838ded9158600051265cfd82f29f
Bill MacKenty <bmackenty@users.noreply.github.com>
2022-08-03 17:52:51 +0200

    Update proc_gen_weapon_names.py

--- proc_gen_weapon_names.py
+++ proc_gen_weapon_names.py
@@ -3,7 +3,7 @@ import random
 
 weapon_types = ["long sword", "short sword", "dagger", "short bow", "long bow", "staff", "mace", "morning star", "crossbow", "dart", "rapier", "great axe", "two-handed sword"]
 weapon_prefix = ["sharp", "broken", "exquisite", "normal", "sentient", "flaming", "frost", "furious", "glowing", "hot", "icy", "lightning", "magic", "mighty", "mystic", "poisoned", "sharp", "silent", "slimy", "sneaky", "sparkling", "thundering", "twilight", "vicious", "wicked", "opinionated", "wrenching", "zealous"]
-weapon_previous_owner = ["Votran's", "Anna's", "Gorg's"]
+weapon_previous_owner = ["Votran's", "Anna's", "Gorg's", "Ar'rian's", "Thag's", "Quein's", "Worble's"]
 
 for i in range(10):
     weapon_name = random.choice(weapon_previous_owner) + " +" + str(random.randint(1,3)) + " " + random.choice(weapon_prefix) + " " + random.choice(weapon_types)
