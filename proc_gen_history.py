a67f05a9e85e63a6cd6959bef4d76e914074984c
Bill MacKenty <bmackenty@users.noreply.github.com>
2022-08-04 20:37:58 +0200

    Update proc_gen_history.py

--- proc_gen_history.py
+++ proc_gen_history.py
@@ -5,6 +5,8 @@ origin_1 = ["Born", "Hatched", "Invoked", "Discovered"]
 origin_2 = ["in the land of", "in the wilds of", "in the forest of", "in the ocean of", "in the small village of", "in the modest hamlet of"]
 origin_3 = ["Tr'lor", "Kor'mer", "Kobiyashi", "Greenest", "Mordora", "Gondor'e", "Rivendell", "Mirkwood"]
 
-for i in range(10):
-    story = random.choice (origin_1) + " " + random.choice(origin_2) + " " + random.choice(origin_3)
-    print(story)
+story_part_1 = random.choice (origin_1) + " " + random.choice(origin_2) + " " + random.choice(origin_3)
+print(story_part_1)
+
+origin_4 = ["a young male child", "a young female child", "a young child"]
+origin_5 = ["came into being."]
