a5eba088568fa53673c4ce184bb3349343c461cb
Bill MacKenty <bmackenty@users.noreply.github.com>
2022-08-05 13:39:59 +0200

    Update proc_gen_weapon_names.py

--- proc_gen_weapon_names.py
+++ proc_gen_weapon_names.py
@@ -28,28 +28,30 @@ selected_weapon_magic = random.choice(weapon_types_magic)
 weapon_prefix = ["sharp", "broken", "exquisite", "normal", "sentient", "flaming", "frosty", "furious", "glowing", "hot", "icy", "lightning", "magic", "mighty", "mystic", "poisoned", "sharp", "silent", "slimy", "sneaky", "sparkling", "thundering", "twilight", "vicious", "wicked", "opinionated", "wrenching", "zealous"]
 selected_weapon_prefix = random.choice(weapon_prefix)
 
-if selected_weapon_culture == "Dwarven":
-    dwarven_names = ["Graddag Granitefall", "Baridgrami Ashdelver", "Erirnoick Orehead", "Theddumlin Bottlebrand", "Hastom Flintgut", "Brourhal Warmgrog", "Lovraic Bitterbreaker", "Bhagrec Leathercloak", "Nukdruth Lightcloak","Vosgrolin Bloodshield"]
-    selected_weapon_previous_owner = random.choice(dwarven_names)
-elif selected_weapon_culture == "Elvish":
-    elvish_names = ["Iyrandrar Sarmaer","Haryk Olayarus","Elaith Theran","Felaern Enna","Alluin Olofiel","Hagduin Nerirora","Otaehryn Keathyra","Ryo Pawenys","Phaendar Keabalar","Iolas Iarkrana"]
-    selected_weapon_previous_owner = random.choice(elvish_names)
-elif selected_weapon_culture == "Human":
-    human_names = ["Boron","Celestine","Dagmar","Eleanor","Fiona","Gillian","Hannah","Irene","Jasmine","Katherine","Lillian","Maggie","Natalie","Olivia","Pamela","Quinn","Rachel","Samantha","Tanya","Una","Vivian","Wendy","Xaviera","Yvonne","Zoe"]
-    selected_weapon_previous_owner = random.choice(human_names)
-elif selected_weapon_culture == "Infernal":
-    infernal_names = ["Boron","Celestine","Eleanor","Fiona","Gillian","Hannah","Irene","Jasmine","Katherine","Lillian","Maggie","Natalie","Olivia","Pamela","Quinn","Rachel","Samantha","Tanya","Una","Vivian","Wendy","Xaviera","Yvonne","Zoe"]
-    selected_weapon_previous_owner = random.choice(infernal_names)
-elif selected_weapon_culture == "Gnomish":
-    gnomish_names = ["Ipawor Paleback","Sinmin Bellyreach","Yosston Singlespell","Warryn Flukecase","Poybar Sparklecollar","Lokur Dampletomple","Orumop Pugglorebas","Yosyur Wargebas","Tanzu Wusabade","Jordon Wadlenadle"]
-    selected_weapon_previous_owner = random.choice(gnomish_names)
-elif selected_weapon_culture == "Orcish":
-    orcish_names = ["Zorhgigoth","Lob","Xaakt","Dorgarag","Routhu","Yamarz","Rarfu","Cubub","Nagrub","Urzog","Thagsplitter", "SkullCrusher"]
-    selected_weapon_previous_owner = random.choice(orcish_names)
-else:
-    selected_weapon_previous_owner = "Unknown"
+def weapon_name(culture):
+    print(culture)
+    if culture == "Dwarven":
+        dwarven_names = ["Graddag Granitefall", "Baridgrami Ashdelver", "Erirnoick Orehead", "Theddumlin Bottlebrand", "Hastom Flintgut", "Brourhal Warmgrog", "Lovraic Bitterbreaker", "Bhagrec Leathercloak", "Nukdruth Lightcloak","Vosgrolin Bloodshield"]
+        return random.choice(dwarven_names)
+    elif culture == "Elvish":
+        elvish_names = ["Iyrandrar Sarmaer","Haryk Olayarus","Elaith Theran","Felaern Enna","Alluin Olofiel","Hagduin Nerirora","Otaehryn Keathyra","Ryo Pawenys","Phaendar Keabalar","Iolas Iarkrana"]
+        return random.choice(elvish_names)
+    elif culture == "Human":
+        human_names = ["Boron","Celestine","Dagmara","Eleanor","Fiona","Gillian","Hannah","Irene","Jasmine","Katherine","Lillian","Maggie","Natalie","Olivia","Pamela","Quinn","Rachel","Samantha","Tanya","Una","Vivian","Wendy","Xaviera","Yvonne","Zoe"]
+        return random.choice(human_names)
+    elif culture == "Infernal":
+        infernal_names = ["Boron","Celestine","Eleanor","Fiona","Gillian","Hannah","Irene","Jasmine","Katherine","Lillian","Maggie","Natalie","Olivia","Pamela","Quinn","Rachel","Samantha","Tanya","Una","Vivian","Wendy","Xaviera","Yvonne","Zoe"]
+        return random.choice(infernal_names)
+    elif culture == "Gnomish":
+        gnomish_names = ["Ipawor Paleback","Sinmin Bellyreach","Yosston Singlespell","Warryn Flukecase","Poybar Sparklecollar","Lokur Dampletomple","Orumop Pugglorebas","Yosyur Wargebas","Tanzu Wusabade","Jordon Wadlenadle"]
+        return random.choice(gnomish_names)
+    elif culture == "Orcish":
+        orcish_names = ["Zorhgigoth","Lob","Xaakt","Dorgarag","Routhu","Yamarz","Rarfu","Cubub","Nagrub","Urzog","Thagsplitter", "SkullCrusher"]
+        return random.choice(orcish_names)
+    else:
+        return "Unknown"
 
-weapon_material = ["wood", "stone", "rare metals", "incadescent stone", "silver", "dark matter", "metorite"]
+weapon_material = ["hard wood", "dark wood", "exotic wood", "rare stone", "semi-precious gems", "rare metals", " obsidian", "incadescent stone", "silver", "dark matter", "metorite"]
 
 weapon_alignment = ["lawful", "neutral", "chaotic", "unaligned", "evil"]
 selected_weapon_alignment = random.choice(weapon_alignment)
@@ -63,8 +65,17 @@ selected_weapon_sentience = random.choice(weapon_sentience)
 weapon_size = ["tiny", "small", "medium", "large", "huge"]
 selected_weapon_size = random.choice(weapon_size)
 
+final_weapon_material = ""
+selected_weapon_material = random.sample(weapon_material, random.randint(2,5))
+for i in selected_weapon_material:
+    final_weapon_material = final_weapon_material + ", " + i
+final_weapon_material = final_weapon_material.lstrip(",")
+last_comma = final_weapon_material.rfind(",")
+final_weapon_material = final_weapon_material[:last_comma] + " and" + final_weapon_material[(last_comma+1):]
 
+weapon_story_1 = "Long ago, during the era of the " + selected_weapon_age + ", " + selected_weapon_culture 
+weapon_story_2 = "smiths forged " + weapon_name(selected_weapon_culture) + "'s " + selected_weapon_prefix
+weapon_story_3 = selected_weapon_heavy_melee + ". Constructed in a helix-shaped combination of " + final_weapon_material 
 
+print(weapon_story_1, weapon_story_2, weapon_story_3)
 
-# weapon_name = "Long ago, during the era of " + random.choice(weapon_age) + ", " + random.choice(weapon_culture) + " smiths forged " + random.choice(weapon_previous_owner) + " " + random.choice(weapon_prefix) + " " + random.choice(weapon_types) + ". " + "Forged of " + random.choice(weapon_material) + "," 
-# print(weapon_name)
