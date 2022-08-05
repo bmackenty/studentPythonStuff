c42682e2ec3ff4e86b207a5a975b0b3a7951a47b
Bill MacKenty <bmackenty@users.noreply.github.com>
2022-08-05 12:15:09 +0200

    Update proc_gen_weapon_names.py

--- proc_gen_weapon_names.py
+++ proc_gen_weapon_names.py
@@ -2,14 +2,13 @@
 import random
 
 weapon_culture = ["Dwarven", "Elvish", "Human", "Infernal", "Gnomish", "Orcish"]
-selected_culture = random.choice(weapon_culture)
+selected_weapon_culture = random.choice(weapon_culture)
 
 weapon_age = ["Peace", "War", "Tranquility", "Sundering", "Rage", "Destruction", "Terror", "Doom", "Despair", "Glory", "Victory", "Darkeness", "Light", "Discovery"]
-selected_age = random.choice(weapon_age)
+selected_weapon_age = random.choice(weapon_age)
 
 weapon_category = ["ranged", "melee", "magic", "thrown", "other"]
 selected_weapon_category = random.choice(weapon_category)
-
 
 weapon_types_ranged = ["short bow", "long bow", "crossbow"]
 selected_weapon_ranged= random.choice(weapon_types_ranged)
@@ -29,20 +28,41 @@ selected_weapon_magic = random.choice(weapon_types_magic)
 weapon_prefix = ["sharp", "broken", "exquisite", "normal", "sentient", "flaming", "frosty", "furious", "glowing", "hot", "icy", "lightning", "magic", "mighty", "mystic", "poisoned", "sharp", "silent", "slimy", "sneaky", "sparkling", "thundering", "twilight", "vicious", "wicked", "opinionated", "wrenching", "zealous"]
 selected_weapon_prefix = random.choice(weapon_prefix)
 
-# weapon_previous_owner = ["Friar's", "Votran's", "Anna's", "Gorg's", "Ar'rian's", "Thag's", "Quein's", "Worble's"]
+if selected_weapon_culture == "Dwarven":
+    dwarven_names = ["Graddag Granitefall", "Baridgrami Ashdelver", "Erirnoick Orehead", "Theddumlin Bottlebrand", "Hastom Flintgut", "Brourhal Warmgrog", "Lovraic Bitterbreaker", "Bhagrec Leathercloak", "Nukdruth Lightcloak","Vosgrolin Bloodshield"]
+    selected_weapon_previous_owner = random.choice(dwarven_names)
+elif selected_weapon_culture == "Elvish":
+    elvish_names = ["Iyrandrar Sarmaer","Haryk Olayarus","Elaith Theran","Felaern Enna","Alluin Olofiel","Hagduin Nerirora","Otaehryn Keathyra","Ryo Pawenys","Phaendar Keabalar","Iolas Iarkrana"]
+    selected_weapon_previous_owner = random.choice(elvish_names)
+elif selected_weapon_culture == "Human":
+    human_names = ["Boron","Celestine","Dagmar","Eleanor","Fiona","Gillian","Hannah","Irene","Jasmine","Katherine","Lillian","Maggie","Natalie","Olivia","Pamela","Quinn","Rachel","Samantha","Tanya","Una","Vivian","Wendy","Xaviera","Yvonne","Zoe"]
+    selected_weapon_previous_owner = random.choice(human_names)
+elif selected_weapon_culture == "Infernal":
+    infernal_names = ["Boron","Celestine","Eleanor","Fiona","Gillian","Hannah","Irene","Jasmine","Katherine","Lillian","Maggie","Natalie","Olivia","Pamela","Quinn","Rachel","Samantha","Tanya","Una","Vivian","Wendy","Xaviera","Yvonne","Zoe"]
+    selected_weapon_previous_owner = random.choice(infernal_names)
+elif selected_weapon_culture == "Gnomish":
+    gnomish_names = ["Ipawor Paleback","Sinmin Bellyreach","Yosston Singlespell","Warryn Flukecase","Poybar Sparklecollar","Lokur Dampletomple","Orumop Pugglorebas","Yosyur Wargebas","Tanzu Wusabade","Jordon Wadlenadle"]
+    selected_weapon_previous_owner = random.choice(gnomish_names)
+elif selected_weapon_culture == "Orcish":
+    orcish_names = ["Zorhgigoth","Lob","Xaakt","Dorgarag","Routhu","Yamarz","Rarfu","Cubub","Nagrub","Urzog","Thagsplitter", "SkullCrusher"]
+    selected_weapon_previous_owner = random.choice(orcish_names)
+else:
+    selected_weapon_previous_owner = "Unknown"
 
 weapon_material = ["wood", "stone", "rare metals", "incadescent stone", "silver", "dark matter", "metorite"]
 
 weapon_alignment = ["lawful", "neutral", "chaotic", "unaligned", "evil"]
 selected_weapon_alignment = random.choice(weapon_alignment)
 
-weapon_personality = ["optimisitic", "pessimistic", "jealous", "bloodthirsty", "uncertain", ""]
+weapon_personality = ["optimisitic", "pessimistic", "jealous", "bloodthirsty", "uncertain", "self-conscious"]
+selected_weapon_personality = random.choice(weapon_personality)
 
 weapon_sentience = ["sentient", "unsentient"]
 selected_weapon_sentience = random.choice(weapon_sentience)
 
 weapon_size = ["tiny", "small", "medium", "large", "huge"]
 selected_weapon_size = random.choice(weapon_size)
+
 
 
 
