657bbd3ed1eaef4a4f2809dd92c8ebfdd5e83a54
Bill MacKenty <bmackenty@users.noreply.github.com>
2022-12-28 17:28:28 +0100

    Create directed_grpah.py

---
+++ directed_grpah.py
@@ -0,0 +1,48 @@
+# This code was NOT WRITTEN BY ME. It was written by ChatAI. 
+# this code creates a weighted directed graph with 10 nodes and random weights between 1 and 10
+import random
+import matplotlib.pyplot as plt
+import networkx as nx
+
+class Graph:
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes
+        self.adj_matrix = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
+
+    def add_edge(self, src, dest, weight):
+        self.adj_matrix[src][dest] = weight
+
+# Create the graph with 100 nodes
+g = Graph(10)
+
+# Add edges to the graph with random weights between 1 and 10
+for i in range(10):
+    num_edges = random.randint(1, 10)
+    for j in random.sample(range(10), num_edges):
+        if i != j:
+            weight = random.randint(1, 10)
+            g.add_edge(i, j, weight)
+
+# Create a NetworkX graph
+nx_graph = nx.DiGraph()
+
+# Add nodes to the graph
+for i in range(10):
+    nx_graph.add_node(i)
+
+# Add edges to the graph
+for i in range(10):
+    for j in range(10):
+        if g.adj_matrix[i][j] > 0:
+            nx_graph.add_edge(i, j, weight=g.adj_matrix[i][j])
+
+# Draw the graph
+pos = nx.circular_layout(nx_graph)
+nx.draw(nx_graph, pos, with_labels=True, font_weight='bold')
+
+# Draw edge labels
+edge_labels = nx.get_edge_attributes(nx_graph, 'weight')
+nx.draw_networkx_edge_labels(nx_graph, pos, edge_labels=edge_labels)
+
+# Show the graph
+plt.show()
