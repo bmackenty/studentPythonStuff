530bca55fb9a7656b4b41c2f7a5ab988c9b4fb0e
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-01-31 16:56:45 +0100

    Update wiz.py

--- wiz.py
+++ wiz.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # Set the screen size
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((900, 700))
 
 # Set the game title
 pygame.display.set_caption("Wizardry 2")
@@ -17,10 +17,10 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Define some rectangles for the UI sections
-status_rect = pygame.Rect(0, 0, 800, 100)
-map_rect = pygame.Rect(0, 100, 400, 400)
-commands_rect = pygame.Rect(400, 100, 400, 400)
-characters_rect = pygame.Rect(0, 500, 800, 100)
+status_rect = pygame.Rect(5, 5, 890, 100)
+map_rect = pygame.Rect(5, 120, 430, 400)
+commands_rect = pygame.Rect(450, 120, 445, 400)
+characters_rect = pygame.Rect(5, 530, 890, 155)
 
 
 # Main game loop
@@ -39,18 +39,32 @@ while running:
     pygame.draw.rect(screen, WHITE, commands_rect, 2)
     pygame.draw.rect(screen, WHITE, characters_rect, 2)
 
+
+
+
     # Render some text to the screen
     text = font.render("Status", True, WHITE)
     screen.blit(text, (10, 10))
 
     text = font.render("Map", True, WHITE)
-    screen.blit(text, (10, 110))
+    screen.blit(text, (10, 130))
 
     text = font.render("Commands", True, WHITE)
-    screen.blit(text, (410, 110))
+    screen.blit(text, (458, 130))
 
     text = font.render("Characters", True, WHITE)
-    screen.blit(text, (10, 600-90))
+    screen.blit(text, (10, 600-80))
+        # Render text to the characters section
+    text = font.render("Character 1", True, WHITE)
+    screen.blit(text, (10, 550))
+        # Render text to the characters section
+    text = font.render("Character 2", True, WHITE)
+    screen.blit(text, (10, 570))
+    text = font.render("Character 3", True, WHITE)
+    screen.blit(text, (10, 590))
+    text = font.render("Character 4", True, WHITE)
+    screen.blit(text, (10, 610))
+    
     # Update the screen
     pygame.display.flip()
 
