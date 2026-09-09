a6434b107b19978f820db033eaaa8dac050d0619
Bill MacKenty <bmackenty@users.noreply.github.com>
2026-09-09 08:35:53 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,3 @@
-import os
 
 while True:
     print("1. this is for choice 1")
@@ -14,13 +13,15 @@ while True:
     if choice == "1": 
         print("I'm doing stuff for choice 1")
         any_key = input("Press enter to continue...")
-        os.system("clear")
     elif choice == "2":
         print("I'm doing stuff for choice 2")
         any_key = input("Press enter to continue...")
-        os.system("clear")
-
     elif choice == "3":
         print("I'm doing stuff for choice 3")
         any_key = input("Press enter to continue...")
-        os.system("clear")
+    elif choice == "4":
+        print("I'm doing stuff for choice 4")
+        any_key = input("Press enter to continue...")
+    elif choice == "5":
+        print("I'm doing stuff for choice 5")
+        any_key = input("Press enter to continue...")
